3c3a59cb5d10f15aa93142fff02a0a1399a79bd9
ibrahimshahid1 <121583863+ibrahimshahid1@users.noreply.github.com>
2024-10-11 11:32:09 -0400

    10/11/2024

---
+++ Modulefive/modulefivelessonfour/modulefivelessonfour/ibrahimshahidlesson4.java
@@ -0,0 +1,19 @@
+
+/**
+ * Write a description of class ibrahimshahidlesson4 here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+import java.io.*;
+import java.util.*;
+public class ibrahimshahidlesson4
+{
+    public static void main(String[]args){
+        Scanner constanta = new Scanner(System.in);
+        System.out.println("Enter constant a");
+        double constanta = constanta.nextDouble();
+        Scanner constantb = new Scanner(System.in);
+        System.out.println("Enter Constant b");
+    }
+}
